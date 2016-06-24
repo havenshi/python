283ae826bfb7293af288068e76d148f6ec84feeb
haven shi <haiwen.shi01@gmail.com>
2016-06-23 22:06:24 -0500

    ch09

--- ch03.py
+++ ch03.py
@@ -23,3 +23,11 @@ def cat_n_times(s,n):
 	print s*n
 
 cat_n_times('Spam',7)
+
+#test input and raw_input
+a=input()
+print a    #'a'>a, 1>1, h>error
+
+b=raw_input()
+print a    #'a'>'a', 1>1, h>h
+
